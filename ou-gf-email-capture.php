fb0946c8829b60d62fefed2f850a2324d7033600
Diego Garza <dgarza8988@gmail.com>
2018-08-08 22:32:55 +0300

    upped version

--- ou-gf-email-capture.php
+++ ou-gf-email-capture.php
@@ -6,7 +6,7 @@
  *
  * Plugin Name: OU GF Email Capture
  * Description: Adds email subscribe field connected to silverpop to gravity forms
- * Version: 0.5
+ * Version: 0.6
  * Author: Yocheved Blum
  */
 
